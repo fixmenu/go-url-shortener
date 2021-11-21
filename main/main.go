0965dd50030130a11d694f489f8e9e8c7ef2ce3f
mert gönkesen <mert@yourporter.com>
2021-11-21 16:16:21 +0300

    default  map handler and redirect

--- main/main.go
+++ main/main.go
@@ -7,15 +7,39 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
-	})
+	mux := defaultMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi!")
-	})
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	pathHandler := mapHandler(pathsToUrls, mux)
+
+	if err := http.ListenAndServe(":8080", pathHandler); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func mapHandler(paths map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if dest, ok := paths[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", greetHandler)
+	return mux
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Greetings!")
 }
